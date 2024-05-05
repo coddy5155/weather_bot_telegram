f5b77bef0ea412b853dbc2936d6dee020ea000c3
coddy5155 <mega.slowler@gmail.com>
2024-05-05 08:53:08 +0500

    111

--- weather_bot.py
+++ weather_bot.py
@@ -63,14 +63,11 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger.addHandler(fh)
 
 def main() -> None:
-
+#new addition
     application = Application.builder().token("TOKEN").build()
-
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
